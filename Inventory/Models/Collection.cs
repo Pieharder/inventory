c8ea484ba4af53e6ce7e8a1f18fdabe1fbd28853
Brandan Sayarath <brandan@brandan.tech>
2020-03-17 11:54:36 -0700

    add collection and inventorycontext models
    co-authored by: Michelle Morin <michelle-morin>
    co-authored by: Geoff Goetz <geoff.goetz503@gmail.com>

---
+++ Inventory/Models/Collection.cs
@@ -0,0 +1,9 @@
+namespace Inventory.Models
+{
+  public class Collection
+  {
+    public int CollectionId { get; set; }
+    public string Type { get; set; }
+    public string Description { get; set; }
+  }
+}
